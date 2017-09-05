954a2b0a06d19bc606f5259b81a9f7adf855484b
adrian popovici <adrian.popovici@cst-dev.com>
2017-09-05 11:18:27 +0300

    Added activity_login layout

---
+++ app/src/main/java/prastiebros/com/chatdeprastie/LoginActivity.java
@@ -0,0 +1,19 @@
+package prastiebros.com.chatdeprastie;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by adrianpopovici on 9/4/17.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}

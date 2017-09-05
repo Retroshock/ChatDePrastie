1d2dc9e83ecf624f8452d12540b64f9505b70071
adrian popovici <adrian.popovici@cst-dev.com>
2017-09-05 11:37:55 +0300

    working on login

--- app/src/main/java/prastiebros/com/chatdeprastie/LoginActivity.java
+++ app/src/main/java/prastiebros/com/chatdeprastie/LoginActivity.java
@@ -1,7 +1,12 @@
 package prastiebros.com.chatdeprastie;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 /**
  * Created by adrianpopovici on 9/4/17.
@@ -9,10 +14,27 @@ import android.support.v7.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    System.out.println("User signed in! " + user.getUid());
+                } else {
+                    // User is signed out
+                    System.out.println("User signed out! ");
+                }
+            }
+        };
     }
 
 
